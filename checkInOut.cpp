04d0bd6c3bfef9c884f182d51d509feb6afaae45
yonaSisay <86576073+yonaSisay@users.noreply.github.com>
2021-09-04 00:49:32 +0300

    Update checkInOut.cpp

--- checkInOut.cpp
+++ checkInOut.cpp
@@ -7,20 +7,25 @@ using namespace std;
 time_t now = time(0);
 tm *ltm = localtime(&now);
 
+int roomNo;
+
+struct order{
+    order() : status(true) {};
+    char name[20];
+    char address[20];
+    char phone[10];
+    bool status;    
+} room[];
+
+
 struct Date
 {
    int Day; 
    int month;
-    int year;
+   int year;
 };
-
-bool ReFre;
-
-Date checkIn;
-Date checkOut;
-Date NetTime;
+Date checkIn[50], checkOut[50], NetTime[50];
 Date currentTime = {ltm->tm_mday,1+ltm->tm_mon,1900+ltm->tm_year};
-
 
 struct Return
    {
@@ -28,7 +33,7 @@ struct Return
        Date chOu;
    };
 
-Return dates;
+Return dates[50];
 
 Return checkInOut(){
     clrscr();
@@ -36,51 +41,58 @@ Return checkInOut(){
     cout<<"\t\t\t"<<"Enter the Day of reservation"<<endl;
     cout<<"\t\t\t"<<"***********************************"<<endl;
     cout<<"\t\t\t";
-    cin >>checkIn.Day;
+    cin >>checkIn[50].Day;
     cout<<"\t\t\t"<<"***********************************"<<endl;
     cout<<"\t\t\t"<<"Enter the month of reservation"<<endl;
     cout<<"\t\t\t"<<"***********************************"<<endl;
     cout<<"\t\t\t";
-    cin >> checkIn.month;
+    cin >> checkIn[50].month;
     cout<<"\t\t\t"<<"***********************************"<<endl;
     cout<<"\t\t\t"<<"Enter the year of reservation"<<endl;
     cout<<"\t\t\t"<<"***********************************"<<endl;
     cout<<"\t\t\t";
-    cin >> checkIn.year;
+    cin >> checkIn[50].year;
     cout<<"\t\t\t"<<"***********************************"<<endl;
-    cout<<"\t\t\t"<<"Enter the day of living the room room"<<endl;
-    cout<<"\t\t\t"<<"***********************************"<<endl;
-    cout<<"\t\t\t";
-    cin>> checkOut.Day;
-    cout<<"\t\t\t"<<"***********************************"<<endl;
-    cout<<"\t\t\t"<<"Enter the month of living the room"<<endl;
+    cout<<"\t\t\t"<<"Enter the day of clearing froom the room"<<endl;
     cout<<"\t\t\t"<<"***********************************"<<endl;
     cout<<"\t\t\t";
-    cin>> checkOut.month;
+    cin>> checkOut[50].Day;
+    cout<<"\t\t\t"<<"***********************************"<<endl;
+    cout<<"\t\t\t"<<"Enter the month of clearing from the room"<<endl;
+    cout<<"\t\t\t"<<"***********************************"<<endl;
+    cout<<"\t\t\t";
+    cin>> checkOut[50].month;
    
-    dates.chIn = checkIn;
-    dates.chOu = checkOut;
+    dates[50].chIn = checkIn[50];
+    dates[50].chOu = checkOut[50];
  
-    return dates;
+    return dates[50];
 };
 
-bool terminate (Date currentTime, Return dates , bool ReFre){
-        if(currentTime.Day > dates.chOu.Day && currentTime.month > dates.chOu.Day){
-            ReFre = 0;//room is free
-        }else if(currentTime.Day > dates.chOu.Day && currentTime.month < dates.chOu.Day){
-            ReFre = 1;//room is occupied
-        }else if(currentTime.Day < dates.chOu.Day && currentTime.month > dates.chOu.Day){
-            ReFre = 1;//room is occupied
-        }else if(currentTime.Day < dates.chOu.Day && currentTime.month < dates.chOu.Day) {
-            ReFre = 1;//room is occupied
+bool terminate (Date currentTime, Return dates[], order room[50]){
+    for (int i=0; i<50; i++){
+        if(currentTime.Day > dates[i].chOu.Day && currentTime.month > dates[i].chOu.Day){
+            room[i].status = 0;//room is free
+        }else if(currentTime.Day > dates[i].chOu.Day && currentTime.month < dates[i].chOu.Day){
+            room[i].status = 1;//room is occupied
+        }else if(currentTime.Day < dates[i].chOu.Day && currentTime.month > dates[i].chOu.Day){
+            room[i].status = 1;//room is occupied
+        }else if(currentTime.Day < dates[i].chOu.Day && currentTime.month < dates[i].chOu.Day) {
+            room[i].status= 1;//room is occupied
         }
-    return ReFre;
+    }       
+    return room;
 }
 
 
 
+
+
+
 int main(){
+
 checkInOut();
-terminate(currentTime, dates, ReFre);
+terminate(currentTime, dates,room);
+
 return 0;
 }
